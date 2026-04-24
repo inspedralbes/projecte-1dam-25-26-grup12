f3402bb4ae43680764bd5f99b364a4db9c8ed15d
sanahussain0325 <sanahussain0325@gmail.com>
2026-04-24 09:53:39 +0200

    landingpage creat

--- php/header.php
+++ php/header.php
@@ -16,3 +16,4 @@
             <p><a href="admin.php">Admin</a></p>
             <p><a href="tecnic.php">Tècnic</a></p>
         </div>
+        <hr>
--- php/usuari.php
+++ php/usuari.php
@@ -16,8 +16,7 @@ require_once 'header.php';
 
 
 <body>
-    <div id="menu">
-        <hr>
+    <div id="menu" style="width: 300px; margin: 0 auto;">
         <p><a href="formulari.php">Registrar nova incidència</a></p>
         <p><a href="mostrar.php">Veure estat incidència</a></p>
     </div>
