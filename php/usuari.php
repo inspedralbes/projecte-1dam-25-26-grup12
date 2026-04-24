f5dd09ffb33e82e8aa681bb1337dd48d15cbe2c8
sanahussain0325 <sanahussain0325@gmail.com>
2026-04-24 10:48:11 +0200

    estat creat

--- php/usuari.php
+++ php/usuari.php
@@ -18,7 +18,7 @@ require_once 'header.php';
 <body>
     <div id="menu" style="width: 300px; margin: 0 auto;">
         <p><a href="formulari.php">Registrar nova incidència</a></p>
-        <p><a href="mostrar.php">Veure estat incidència</a></p>
+        <p><a href="estado.php">Veure estat incidència</a></p>
     </div>
 
 </body>
