fec7c66f7891195d78d055de96e2afdd35c17f0f
a25adrurbrub <a25adrurbrub@inspedralbes.cat>
2026-05-04 12:02:35 +0200

    documentacio de llistar

--- php/llistar.php
+++ php/llistar.php
@@ -1,43 +1,51 @@
 <?php
 
-//Sempre volem tenir una connexió a la base de dades, així que la creem al principi del fitxer
+//conexio a la base de dades
 require_once 'connexio.php';
+//header
 require_once 'header.php';
-// Un cop inclòs el fitxer connexio.php, ja podeu utilitzar la variable $conn per a fer les consultes a la base de dades.
 
+//seccio de consultes SQL
+
+    //gets per ordenar les incidencies 
     $sort = $_GET['sort'] ?? 'prioridad';
-
     $order = $_GET['order'] ?? 'desc';
 
+    //gets per ordenar les incidencies no assigandes
     $sort1 = $_GET['sort1'] ?? 'fecha';
-
     $order1 = $_GET['order1'] ?? 'desc';
 
-    // Consulta SQL per obtenir totes les files de la taula 'cases'
+    // Consulta SQL per obtenir  les files de la taula incidencies que tinguin prioritat assignada
+    // i que no tinguin data de finalització (incidencies pendents)(te un JOIN amb tipo tecnic i departament per mostrar els noms en comptes dels ids)
     $sql = "SELECT i.id_incidencia, i.descripcio, i.fecha, d.nom AS departament_nom, t.nom AS tipologia_nom, i.prioridad, tec.nom AS tecnic_nom
     FROM INCIDENCIA AS i LEFT JOIN DEPARTAMENT AS d ON i.id_dept=d.id_dept LEFT JOIN TIPO AS t ON i.id_tipo=t.id_tipo  
     LEFT JOIN TECNIC AS tec ON i.id_tecnic=tec.id_tecnic WHERE fecha_fin IS NULL AND prioridad IS NOT NULL  ORDER BY $sort $order";
 
+    // Executar la consulta i obtenir els resultats
     $result = $conn->query($sql);
 
+    // Consulta SQL per obtenir  les files de la taula incidencies que no tinguin prioritat assignada
+    // i que no tinguin data de finalització (incidencies pendents)(te un JOIN amb tipo tecnic i departament per mostrar els noms en comptes dels ids)
     $sql1 = "SELECT i.id_incidencia, i.descripcio, i.fecha, d.nom AS departament_nom, t.nom AS tipologia_nom, i.prioridad, tec.nom AS tecnic_nom
     FROM INCIDENCIA AS i LEFT JOIN DEPARTAMENT AS d ON i.id_dept=d.id_dept LEFT JOIN TIPO AS t ON i.id_tipo=t.id_tipo  
     LEFT JOIN TECNIC AS tec ON i.id_tecnic=tec.id_tecnic WHERE fecha_fin IS NULL AND prioridad IS  NULL  ORDER BY $sort1 $order1";
 
-
+    // Executar la consulta i obtenir els resultats
     $result1 = $conn->query($sql1);
 
 
-    // Comprovar si hi ha resultats
+    // Comprovar si hi ha resultats per a les incidencies pendents sense prioritat assignada i sino no mostra resulats
     if ($result1->num_rows > 0) {
         ?>
          <h1>Incidencies Pendents</h1>
         
+    <!--Seccio de botons per ordenar -->
         <th>
             Data
             <a class="btn btn-primary" href="?sort1=fecha&order1=asc">↑</a>
             <a class="btn btn-primary" href="?sort1=fecha&order1=desc">↓</a>
         </th>
+        <!-- Taula per mostrar les incidencies pendents sense prioritat assignada -->
         <table class="table table-striped table-dark">
             <tr >
                 <th>ID</th>
@@ -51,7 +59,8 @@ require_once 'header.php';
                 <th>Esborrar</th>
             </tr>
         <?php
-        // Llistar els resultats. ATENCIÓ, heu de construir el codi HTML d'una llista correctament
+
+        // bucle  per mostrar les dades en format taula
         while ($row = $result1->fetch_assoc()) { 
            ?>
             <tr>
@@ -79,7 +88,7 @@ require_once 'header.php';
 
 
 
-    // Comprovar si hi ha resultats
+    // // Comprovar si hi ha resultats per a les incidencies pendents sense prioritat assignada i sino no mostra resulats i et dona un missatge
     if ($result->num_rows > 0) {
         ?>
         <h1>Llistat de Incidencies</h1>
@@ -94,6 +103,7 @@ require_once 'header.php';
             <a class="btn btn-primary" href="?sort=fecha&order=desc">↓</a>
         </th>
         <table class="table table-striped table-dark">
+            <!--Seccio de botons per ordenar -->
             <tr >
                 <th>ID</th>
                 <th>Descripcio</th>
@@ -106,8 +116,9 @@ require_once 'header.php';
                 <th>Esborrar</th>
             </tr>
         <?php
-        // Llistar els resultats. ATENCIÓ, heu de construir el codi HTML d'una llista correctament
+        // bucle  per mostrar les dades en format taula
         while ($row = $result->fetch_assoc()) { 
+            // Condicional per pintar les files de la taula de diferents colors segons la prioritat de la incidencia
             if ($row["prioridad"] == "alta" ){
             ?>
                 <tr class="table-danger">
