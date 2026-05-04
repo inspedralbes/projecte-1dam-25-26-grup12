6d30bb3b4b99f99dd5994429b20ef447a8182b1b
a25adrurbrub <a25adrurbrub@inspedralbes.cat>
2026-05-04 11:41:48 +0200

    llista d'incidencias separada en no asignadas i en pendents

--- php/llistar.php
+++ php/llistar.php
@@ -5,40 +5,93 @@ require_once 'connexio.php';
 require_once 'header.php';
 // Un cop inclòs el fitxer connexio.php, ja podeu utilitzar la variable $conn per a fer les consultes a la base de dades.
 
-?>
+    $sort = $_GET['sort'] ?? 'prioridad';
 
-    <h1>Llistat de Incidencies</h1>
-    <?php
+    $order = $_GET['order'] ?? 'desc';
 
-    $sort = $_GET['sort'] ?? 'id_incidencia';
+    $sort1 = $_GET['sort1'] ?? 'fecha';
 
-    $order = $_GET['order'] ?? 'ASC';
+    $order1 = $_GET['order1'] ?? 'desc';
 
     // Consulta SQL per obtenir totes les files de la taula 'cases'
     $sql = "SELECT i.id_incidencia, i.descripcio, i.fecha, d.nom AS departament_nom, t.nom AS tipologia_nom, i.prioridad, tec.nom AS tecnic_nom
     FROM INCIDENCIA AS i LEFT JOIN DEPARTAMENT AS d ON i.id_dept=d.id_dept LEFT JOIN TIPO AS t ON i.id_tipo=t.id_tipo  
-    LEFT JOIN TECNIC AS tec ON i.id_tecnic=tec.id_tecnic WHERE fecha_fin IS NULL ORDER BY $sort $order";
-
-
-
-
-
-
+    LEFT JOIN TECNIC AS tec ON i.id_tecnic=tec.id_tecnic WHERE fecha_fin IS NULL AND prioridad IS NOT NULL  ORDER BY $sort $order";
 
     $result = $conn->query($sql);
+
+    $sql1 = "SELECT i.id_incidencia, i.descripcio, i.fecha, d.nom AS departament_nom, t.nom AS tipologia_nom, i.prioridad, tec.nom AS tecnic_nom
+    FROM INCIDENCIA AS i LEFT JOIN DEPARTAMENT AS d ON i.id_dept=d.id_dept LEFT JOIN TIPO AS t ON i.id_tipo=t.id_tipo  
+    LEFT JOIN TECNIC AS tec ON i.id_tecnic=tec.id_tecnic WHERE fecha_fin IS NULL AND prioridad IS  NULL  ORDER BY $sort1 $order1";
+
+
+    $result1 = $conn->query($sql1);
+
+
+    // Comprovar si hi ha resultats
+    if ($result1->num_rows > 0) {
+        ?>
+         <h1>Incidencies Pendents</h1>
+        
+        <th>
+            Data
+            <a class="btn btn-primary" href="?sort1=fecha&order1=asc">↑</a>
+            <a class="btn btn-primary" href="?sort1=fecha&order1=desc">↓</a>
+        </th>
+        <table class="table table-striped table-dark">
+            <tr >
+                <th>ID</th>
+                <th>Descripcio</th>
+                <th>Data</th>
+                <th>Departament</th>
+                <th>Tipologia</th>
+                <th>Prioritat</th>
+                <th>Tecnic</th>
+                <th>Modificar</th>
+                <th>Esborrar</th>
+            </tr>
+        <?php
+        // Llistar els resultats. ATENCIÓ, heu de construir el codi HTML d'una llista correctament
+        while ($row = $result1->fetch_assoc()) { 
+           ?>
+            <tr>
+                <td><?= $row["id_incidencia"] ?></td>
+                <td><?= htmlspecialchars($row["descripcio"]) ?> </td>
+                <td><?= $row["fecha"] ?></td>
+                <td><?= $row["departament_nom"] ?></td>
+                <td><?= $row["tipologia_nom"] ?></td>
+                <td><?= $row["prioridad"] ?></td>
+                <td><?= $row["tecnic_nom"] ?></td>
+                <td><a class="btn btn-primary" href='modificar.php?id_incidencia= <?= $row["id_incidencia"] ?> '>Modificar</a></td>
+                <td><a class="btn btn-danger" href='esborrar.php?id_incidencia= <?= $row["id_incidencia"] ?> '>Esborrar</a></p></td>
+  
+            </tr>
+        <?php
+                   
+                
+        }
+        ?>
+        </table>
+        <hr>
+        <?php
+        }
+
+
+
 
     // Comprovar si hi ha resultats
     if ($result->num_rows > 0) {
         ?>
+        <h1>Llistat de Incidencies</h1>
         <th>
             Prioritat
-            <a href="?sort=prioridad&order=asc">↑</a>
-            <a href="?sort=prioridad&order=desc">↓</a>
+            <a class="btn btn-primary" href="?sort=prioridad&order=asc">↑</a>
+            <a class="btn btn-primary" href="?sort=prioridad&order=desc">↓</a>
             </th>
             <th>
             Data
-            <a href="?sort=fecha&order=asc">↑</a>
-            <a href="?sort=fecha&order=desc">↓</a>
+            <a class="btn btn-primary" href="?sort=fecha&order=asc">↑</a>
+            <a class="btn btn-primary" href="?sort=fecha&order=desc">↓</a>
         </th>
         <table class="table table-striped table-dark">
             <tr >
