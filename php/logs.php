4d4f0b3c79a2d88bd7adfa699d6aa2898fb8affa
hajweria.hussain <hajweriahussain15@gmail.com>
2026-05-10 00:18:35 +0200

    filtre creat

--- php/logs.php
+++ php/logs.php
@@ -6,112 +6,110 @@ $client = new MongoDB\Client("mongodb://root:example@mongo:27017/?authSource=adm
 
 $collection = $client->demo->users;
 
-// Hem comptat tots els documents de la col·lecció perquè cada document es igual a un accés.
 $accessos_total = $collection->countDocuments();
 
-// Agrupem els accessos per URI i comptem quantes vegades apareix cada pàgina.
 $pagines = $collection->aggregate([
-
     [
         '$group' => [
-            // Agrupem pel camp uri
             '_id' => '$uri',
-            // Amb la funcio de suma fem un contador que suma 1 per cada document.
             'total' => ['$sum' => 1]
         ]
     ],
-
     [
-        // Ordenem de més visitades a menys visitades.
         '$sort' => ['total' => -1]
     ],
     [
-        // Mostrem només les 10 primeres pagines.
         '$limit' => 10
     ]
 ]);
 
-
 $accessos_dia = $collection->aggregate([
-
     [
         '$group' => [
-            //Com que la varibale date té el format Y-m-d H:i:s, amb la funcio substr sol agafem la data i no les temps.
             '_id' => ['$substr' => ['$date', 0, 10]],
-
-            //El contador que suma 1 cada vegada.
             'total' => ['$sum' => 1]
         ]
     ],
-
     [
-        //Ordenar de més a menys recents.
         '$sort' => ['_id' => 1]
     ]
-
 ]);
+
+$data = $_GET['data'] ?? '';
+$pagina = $_GET['pagina'] ?? '';
+
+$pipeline = [];
+
+$pipeline[] = [
+    '$project' => [
+        'dia' => ['$substr' => ['$date', 0, 10]],
+        'uri' => 1
+    ]
+];
+
+$filtre = [];
+if (!empty($data))   { 
+    $filtre['dia'] = $data; 
+}
+if (!empty($pagina)) { 
+    $filtre['uri'] = $pagina; 
+}
+if (!empty($filtre)) {
+    $pipeline[] = [
+        '$match' => $filtre
+    ]; 
+}
+
+$pipeline[] = [
+    '$group' => [
+        '_id' => ['dia' => '$dia','pagina' => '$uri'],
+        'accessos' => ['$sum' => 1]
+    ]
+];
+
+$pipeline[] = [
+    '$sort' => ['_id.dia' => -1]
+];
+
+$resultat = $collection->aggregate($pipeline);
 
 ?>
 
 <h1>Estadístiques d'Accès</h1>
 
-<h2>Total d'accessos : </h2>
+<h2>Total d'accessos:</h2>
 <?= $accessos_total ?>
-<br>
+
 <h2>Pàgines més visitades</h2>
 
 <table>
-
     <tr>
         <th>Pàgina</th>
         <th>Total visites</th>
     </tr>
 
-    <?php foreach ($pagines as $pagina): ?>
-
+    <?php foreach ($pagines as $enllaç): ?>
         <tr>
-
-            <td>
-                <?= $pagina['_id'] ?>
-            </td>
-
-            <td>
-                <?= $pagina['total'] ?>
-            </td>
-
+            <td><?= $enllaç['_id'] ?></td>
+            <td><?= $enllaç['total'] ?></td>
         </tr>
-
     <?php endforeach; ?>
-
 </table>
 
 <h2>Accessos agrupats per dia</h2>
 
 <table>
-
     <tr>
         <th>Dia</th>
         <th>Total visites</th>
     </tr>
 
     <?php foreach ($accessos_dia as $dia): ?>
-
         <tr>
             <td><?= $dia['_id'] ?></td>
             <td><?= $dia['total'] ?></td>
         </tr>
-
     <?php endforeach; ?>
 </table>
 
 <br>
-
-
-
-
-
-
-
-
-
-
