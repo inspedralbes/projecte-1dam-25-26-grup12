f49ed775837f81c23690931149ee8d7142f0fc56
a25adrurbrub <a25adrurbrub@inspedralbes.cat>
2026-05-12 11:58:17 +0200

    Metode loging ficat a index.php

--- php/index.php
+++ php/index.php
@@ -1,7 +1,52 @@
 <?php
+session_start();
+$error = "";
+require_once 'connexio.php';
+
+
+if (isset($_SESSION["email"])) {
+    if($_SESSION['rol'] == "admin"){
+        header("Location: admin.php");
+    }elseif($_SESSION['rol'] == "user"){
+        header("Location: usuari.php");
+    }elseif($_SESSION['rol'] == "tecnic"){
+        header("Location: tecnic.php");
+    }
+
+    exit();
+}
+
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $usuari= $_POST["email"];
+    $password = $_POST["password"];
+
+    $sql = "SELECT * FROM USERS WHERE email = ?";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("s", $usuari);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $row = $result->fetch_assoc(); 
+
+    if($row['email'] == $usuari){
+        if($row['pass'] == $password){
+            $_SESSION['email'] = $usuari;
+            $_SESSION['rol'] = $row['rol'];
+            header("Location: index.php");
+        }else{
+            $error = "Contraseña incorrecta";
+        }
+        
+    }else{
+        $error = "No existeix l'usuari";
+    }
+
+}
+
 
 //Sempre volem tenir una connexió a la base de dades, així que la creem al principi del fitxer
-require_once 'connexio.php';
+
 require_once 'header.php' ;
 include_once 'mongo.php';
 // Un cop inclòs el fitxer connexio.php, ja podeu utilitzar la variable $conn per a fer les consultes a la base de dades.
@@ -15,9 +60,22 @@ include_once 'mongo.php';
 
         <hr class="mb-4">
         <span class="d-flex justify-content-center gap-3">
-            <a class="btn btn-dark px-4" href="usuari.php">Usuari</a>
-            <a class="btn btn-dark px-4" href="admin.php">Admin</a>
-            <a class="btn btn-dark px-4" href="tecnic.php">Tècnic</a>
+            <h1>Inici de sessió</h1>
+
+            <?php
+            if ($error != "") {
+                echo "<p style='color:red;'>$error</p>";
+            }
+            ?>
+
+            <form method="POST" action="index.php">
+                <label>Usuari:</label><br>
+                <input type="text" name="email" required><br><br>
+                <label>Contrasenya:</label><br>
+                <input type="password" name="password" required><br><br>
+                <button type="submit">Entrar</button>
+
+            </form>
         </span>
 
     </div>
